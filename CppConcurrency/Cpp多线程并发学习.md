2684484f42b4de14c79bf12559fd061c67235d7c
zhongjialin <2974378475@qq.com>
2025-01-13 15:29:21 +0800

    0201

---
+++ CppConcurrency/Cpp多线程并发学习.md
@@ -0,0 +1,4 @@
+《C++并发编程实战（第二版）》部分学习记录
+
+github翻译地址：https://github.com/xiaoweiChen/CPP-Concurrency-In-Action-2ed-2019
+本书源码下载地址：https://www.manning.com/downloads/1954
---
+++ CppConcurrency/Unit2/Part1/list0201.cpp
@@ -0,0 +1,40 @@
+﻿#include <thread>
+#include <iostream>
+
+void do_something(int& i)
+{
+    ++i;
+}
+
+struct func
+{
+    int& i;
+
+    func(int& i_):i(i_){} //i 与 i_ 都是some_local_state 的引用
+
+    void operator()()
+    {
+        for(unsigned j=0;j<100;++j)
+        {
+            do_something(i);  //oops可能提前结束，但是却仍在引用局部变量some_local_state
+            std::cout << i <<'\n';
+        }
+    }
+};
+
+
+void oops()
+{
+    int some_local_state=0;
+    func my_func(some_local_state);
+    std::thread my_thread(my_func);
+    //my_thread.detach();
+    my_thread.join();
+}
+
+int main()
+{
+    oops();
+
+    return 0;
+}
