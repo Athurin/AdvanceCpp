4aea88833c9e2bb665465d1bbd342ccd9dbb66cb
zhongjialin <2974378475@qq.com>
2025-01-15 17:57:20 +0800

    0207批量化管理

---
+++ CppConcurrency/Unit2/Part7/list0207.cpp
@@ -0,0 +1,26 @@
+﻿#include <vector>
+#include <thread>
+#include <algorithm>
+#include <functional>
+#include <iostream>
+
+void do_work(unsigned id)
+{
+    std::cout<<id<<"\n";
+}
+
+void f()
+{
+    std::vector<std::thread> threads;
+    for(unsigned i=0;i<20;++i)
+    {
+        threads.push_back(std::thread(do_work,i));
+    }
+    std::for_each(threads.begin(),threads.end(),
+        std::mem_fn(&std::thread::join)); //用于获取 std::thread 类的 join 成员函数的指针
+}
+
+int main()
+{
+    f();
+}
